190557fc0028a27e1644a0dda2d8c2e2a2c18480
Michael Kennedy <michael@talkpython.fm>
2024-09-11 12:05:18 -0700

    ready to run basics.

--- dockercluster/__init__.py
+++ dockercluster/__init__.py
@@ -3,3 +3,11 @@
 __version__ = '0.1.0'
 __author__ = 'Michael Kennedy <michael@talkpython.fm>'
 __all__ = []
+
+
+def live_status():
+    print("You've been DSed!")
+
+
+if __name__ == '__main__':
+    live_status()
